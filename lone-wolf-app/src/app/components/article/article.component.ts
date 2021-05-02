06ff7d76e298303d7897e02b5a08a23e0dd41c70
nataly-lizano-quiros <72211922+nataly-lizano-quiros@users.noreply.github.com>
2021-05-02 14:56:18 -0600

    article

---
+++ lone-wolf-app/src/app/components/article/article.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent implements OnInit {
+  @Input() text = '';
+  @Input() image = '';
+  @Input() layout = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
